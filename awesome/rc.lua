52aeb4b3916a2b18044374cc4fcddb3e54494bf4
Sebastian Schlag <sebastian.schlag@kit.edu>
2014-05-09 14:50:06 +0200

    now files are tracked

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -382,7 +382,7 @@ for s = 1, screen.count() do
     left_layout:add(mytaglist[s])
     left_layout:add(mypromptbox[s])
 
-    -- Widgets that are aligned to the right foobar
+    -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
     right_layout:add(mylayoutbox[s])
     if s == 1 then
